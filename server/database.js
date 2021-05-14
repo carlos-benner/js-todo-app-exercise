01d6990650d48e8e61c82c0e42d2e7fcd7ae9b6b
Carlos Benner <carlos.benner.b@gmail.com>
2021-05-14 17:35:16 -0400

    Selecting proper DB based on environment mode

--- server/database.js
+++ server/database.js
@@ -1,7 +1,23 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('./database/db.json');
+let db_path;
+
+console.log(`Working on ${process.env.NODE_ENV.toUpperCase()} environment`);
+
+switch (process.env.NODE_ENV.toUpperCase()) {
+    case 'DEVELOPMENT':
+        db_path = './database/dev.db.json';
+        break;
+    case 'TEST':
+        db_path = './database/test.db.json';
+        break;
+    case 'PRODUCTION':
+        db_path = './database/db.json';
+        break;
+}
+
+const adapter = new FileSync(db_path);
 const db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
